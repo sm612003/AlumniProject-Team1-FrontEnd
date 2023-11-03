86434c70a3bbab71df7f4d3a498025aefd41d3ae
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-11-03 14:33:47 +0200

    fetching data for news page

--- technow/src/Components/NewsCard/News.jsx
+++ technow/src/Components/NewsCard/News.jsx
@@ -1,13 +1,13 @@
 import styles from "./News.module.css"
 
-const NewsCard = ({title , image , caption ,alt , first}) => {
+const NewsCard = ({title , image , auther , date , first}) => {
     const firstt = first === true ? styles.SectionFirst : styles.Section ;  
     return(
         <section className={firstt}>
             <h2 className={styles.H2}>{title}</h2>
             <figure className={styles.Figure}>
-                <img src={image} alt={alt} className={styles.Img}/>
-                <caption className={styles.Caption}>{caption}</caption>
+                <img src={`http://localhost:5000/${image}`} alt="newscard img" className={styles.Img}/>
+                <caption className={styles.Caption}>{auther}{date}</caption>
             </figure>                
         </section>
     )
