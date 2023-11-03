4d55f65acd2eaa03a29c812bba99d36cb9c660a4
Issa <isaiahabdulrahman@gmail.com>
2023-11-03 14:46:01 +0200

    deleting hard coded data from blog details component

--- technow/src/Components/BlogDetails/BlogDetailsComponent.jsx
+++ technow/src/Components/BlogDetails/BlogDetailsComponent.jsx
@@ -1,81 +1,20 @@
 import React from "react";
 import styles from "./BlogDetails.module.css"
-import photo from "../../images/pic.jpg"
 
 const BlogDetails = () =>{
     return(
         <div className={styles.blogdetails}>
             <div className={styles.top}>
-                <h1 className={styles.h1}>With the Advent of AI, It's Time To Rethink Human Resources</h1>
-                <p className={styles.p}>By Rob Enderle | September 11, 2023 </p>
+                <h1 className={styles.h1}></h1>
+                <p className={styles.p}></p>
             </div>
             <div className={styles.center}>
                 
-                <img className={styles.image} src={photo} alt="blog image" />
+                <img className={styles.image} src={kjgjhgh} alt="blog image" />
             </div>    
                 <article className={styles.bottom}>
-                    <p className={styles.blogcontent}>We are and will be dealing with several troubling labor actions this year. 
-                         As of this writing, movie writers and actors are on full strike, 
-                         and the United Auto Workers seem to be on track to shut down U.S. domestic automobile production at a time when the car industry is in deep trouble.
-                         Air transportation workers are about to go out on strike as well.
-                         The total impact of these strikes could be devastating on a national level,
-                         with AI and massive compensation disparities between CEOs and other employees cited as major contributing factors.
-                        Human resources (HR) should be one of the protections against this kind of employee collective action. However, for years, 
-                        it has been more of a compliance organization that works to cover up executive mistakes rather than taking care of employees.
-                        AI could have a huge positive impact on work/life balance, 
-                        fair compensation, and turning companies into better workplaces. 
-                        Still, too often, AI is positioned as a threat to employees instead of a benefit.
-                        Let'    s talk about the potential of AI to improve employee-employer relations and help executives make better decisions. 
-                        Then we'll close with my Product of the Week: a new handheld gaming system from Lenovo powered by AMD.
-                        Embracing IBM's AI concept
-                        IBM has been outspoken about using AI to enhance rather than replace employees,
-                        and companies planning to use AI should take this concept to heart.
-                        Take the actors and writers strike, for instance. 
-                        The movie industry has had several very high-priced failures of late. These weren’t the actors’ fault, 
-                        and while you could blame the writers, 
-                        often it was studio decisions that changed the scripts or directors that didn’t seem to understand the subject material that was the cause.
-                        Executives make bad decisions all the time for a variety of reasons. Often, the reason is that they prioritize showcasing 
-                        their authority over someone more qualified to make the decision as some kind of power play.
-                        Assuring the success of movies and TV shows more effectively could substantially reduce the pressure to replace actors and writers with artificial intelligence.
-                        The total impact of these strikes could be devastating on a national level,
-                         with AI and massive compensation disparities between CEOs and other employees cited as major contributing factors.
-                        Human resources (HR) should be one of the protections against this kind of employee collective action. However, for years, 
-                        it has been more of a compliance organization that works to cover up executive mistakes rather than taking care of employees.
-                        AI could have a huge positive impact on work/life balance, 
-                        fair compensation, and turning companies into better workplaces. 
-                        Still, too often, AI is positioned as a threat to employees instead of a benefit.
-                        Let'    s talk about the potential of AI to improve employee-employer relations and help executives make better decisions. 
-                        Then we'll close with my Product of the Week: a new handheld gaming system from Lenovo powered by AMD.
-                        Embracing IBM's AI concept
-                        IBM has been outspoken about using AI to enhance rather than replace employees,
-                        and companies planning to use AI should take this concept to heart.
-                        Take the actors and writers strike, for instance. 
-                        The movie industry has had several very high-priced failures of late. These weren’t the actors’ fault, 
-                        and while you could blame the writers, 
-                        often it was studio decisions that changed the scripts or directors that didn’t seem to understand the subject material that was the cause.
-                        Executives make bad decisions all the time for a variety of reasons. Often, the reason is that they prioritize showcasing 
-                        their authority over someone more qualified to make the decision as some kind of power play.
-                        Assuring the success of movies and TV shows mor
-                        The total impact of these strikes could be devastating on a national level,
-                         with AI and massive compensation disparities between CEOs and other employees cited as major contributing factors.
-                        Human resources (HR) should be one of the protections against this kind of employee collective action. However, for years, 
-                        it has been more of a compliance organization that works to cover up executive mistakes rather than taking care of employees.
-                        AI could have a huge positive impact on work/life balance, 
-                        fair compensation, and turning companies into better workplaces. 
-                        Still, too often, AI is positioned as a threat to employees instead of a benefit.
-                        Let'    s talk about the potential of AI to improve employee-employer relations and help executives make better decisions. 
-                        Then we'll close with my Product of the Week: a new handheld gaming system from Lenovo powered by AMD.
-                        Embracing IBM's AI concept
-                        IBM has been outspoken about using AI to enhance rather than replace employees,
-                        and companies planning to use AI should take this concept to heart.
-                        Take the actors and writers strike, for instance. 
-                        The movie industry has had several very high-priced failures of late. These weren’t the actors’ fault, 
-                        and while you could blame the writers, 
-                        often it was studio decisions that changed the scripts or directors that didn’t seem to understand the subject material that was the cause.
-                        Executives make bad decisions all the time for a variety of reasons. Often, the reason is that they prioritize showcasing 
-                        their authority over someone more qualified to make the decision as some kind of power play.
-                        Assuring the success of movies and TV shows mor
-                        </p>
+                    <p className={styles.blogcontent}>
+                    </p>
                          
                 </article>
             
