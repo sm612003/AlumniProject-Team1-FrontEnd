c1fac2bf9767ee087d221e5ab89b3cea866d94b3
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-11-04 14:35:07 +0200

    transforming the date format

--- technow/src/Components/NewsCard/News.jsx
+++ technow/src/Components/NewsCard/News.jsx
@@ -2,12 +2,18 @@ import styles from "./News.module.css"
 
 const NewsCard = ({title , image , auther , date , first}) => {
     const firstt = first === true ? styles.SectionFirst : styles.Section ;  
+    const formatDate = (date) => {
+        const dateOnly = date.split('T')[0];
+        const [year, month, day] = dateOnly.split('-');
+        return `${day}-${month}-${year}`;
+    }
+
     return(
         <section className={firstt}>
             <h2 className={styles.H2}>{title}</h2>
             <figure className={styles.Figure}>
                 <img src={`http://localhost:5000/${image}`} alt="newscard img" className={styles.Img}/>
-                <caption className={styles.Caption}>{auther}{date}</caption>
+                <caption className={styles.Caption}>{auther}{formatDate(date)}</caption>
             </figure>                
         </section>
     )
