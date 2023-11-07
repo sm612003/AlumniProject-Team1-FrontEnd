949c80767642a9d03433cbe1d2ef828b33867aac
abdulaziz <aboudecharkawi@gmail.com>
2023-11-07 16:19:32 +0200

    handling fetch for add and update blogs and adding news

--- technow/src/Pages/BlogsUpdate/BlogsUpdateForm.jsx
+++ technow/src/Pages/BlogsUpdate/BlogsUpdateForm.jsx
@@ -1,91 +1,177 @@
-import {useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import styles from "./BlogsUpdate.module.css";
-import photo from "../../Assets/Images/Mail.png"
+import photo from "../../Assets/Images/Mail.png";
 import { ScrollButton } from "../../Components/ScrollButton/ScrollButton";
 import { Button } from "../../Components/Buttons/Buttons";
 import axios from "axios";
 
-
 const BlogUpdate = () => {
-
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const [width, setWidth] = useState(screenWidth < 900 ? 'small' : 'big');
 
   useEffect(() => {
-      const handleResize = () => {
-          const newWidth = window.innerWidth;
-          setScreenWidth(newWidth);
-          setWidth(newWidth < 1024 ? 'small' : 'big');
-      };
-      window.addEventListener('resize', handleResize);
-      return () => {
-          window.removeEventListener('resize', handleResize);
-      };
+    const handleResize = () => {
+      const newWidth = window.innerWidth;
+      setScreenWidth(newWidth);
+      setWidth(newWidth < 1024 ? 'small' : 'big');
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
 
-  const formData = new FormData();
-  
-  const formDataObject = {};
-  const image = formData.get("image");
-  if (image.size === 0) {
-    delete formDataObject.image;
-  }
-  axios
-    .patch(`http://localhost:5000/update/blogs`, formDataObject)
-    .then((response) => {
-      console.log("Request was successful:", response.data);
-    })
-    .catch((error) => {
-      console.error("Error while making the request:", error);
+  const { id } = useParams();
+  const [existingData, setExistingData] = useState({});
+  const [inputData, setInputData] = useState({
+    fullname: existingData.author,
+    title: existingData.title,
+    content: existingData.content,
+  });
+  const [imageFile, setImageFile] = useState(existingData.image);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/read/blogsById/${id}`);
+        if (!response.ok) {
+          console.log('error');
+        }
+        setExistingData(response.data);
+        setInputData({
+          fullname: response.data.author,
+          title: response.data.title,
+          content: response.data.content,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  const handleInputChange = (name, value) => {
+    setInputData({
+      ...inputData,
+      [name]: value,
     });
+  };
 
+  const handleImageChange = (e) => {
+    console.log(e.target.files[0])
+    const file = e.target.files[0] || existingData.image;
+    setImageFile(file);
+  };
 
+  const handleImageUpdate = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append("image", file);
+
+      const response = await axios.patch(`http://localhost:5000/update/blogs`,
+      {...formData, id : id});
+
+      console.log("Image updated successfully:", response.data);
+    } catch (error) {
+      console.error("Error updating the image:", error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const updatedData = {
+        author: inputData.fullname,
+        title: inputData.title,
+        content: inputData.content,
+        // Include any other fields that need to be updated.
+      };
+      console.log(updatedData)
+      const response = await axios.patch(`http://localhost:5000/update/blogs`, 
+      {image: imageFile,
+        ...updatedData, id : id },
+      {headers: {'Content-Type' : 'multipart/form-data'}});
+
+      console.log("Blog updated successfully:", response.data);
+
+     
+    } catch (error) {
+      console.error("Error updating the blog:", error);
+    }
+  };
 
   return (
     <div className={styles.Container}>
-       
-
-      <h1 className={styles.h1}>  Update Blog</h1>
+      <h1 className={styles.h1}>Update Blog</h1>
       <div className={styles.Top}>
         <div className={styles.Left}>
-          <form className={styles.form} action="">
-              <label className={styles.name} htmlFor="fullname">Full Name</label>
-              <input className={styles.input} type="text" id="fullname" name="fullname" />
-              
-              <label className={styles.name} htmlFor="title">Title</label>
-              <input  className={styles.input} type="text" id="title" name="title" />
-
-              <div className={styles.Bottom}>
-          <div className={styles.textarea}>
-            <label  className={styles.name} htmlFor="content">Content</label>
-            <textarea className={styles.area} id="content" name="content"></textarea>
-            <div className={styles.inputContainer}>
-            <label className={styles.label}>Enter an image</label>
+          <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+            <label className={styles.name} htmlFor="fullname">
+              Full Name
+            </label>
             <input
               className={styles.input}
-              type="file"
-              name="image"/>
-          </div>
-            
-          </div>
-      
+              type="text"
+              id="fullname"
+              name="fullname"
+              value={inputData.fullname}
+              onChange={(e) => handleInputChange("fullname", e.target.value)}
+            />
 
-        </div>
-              
-              <Button color={"red"} text={"Update Photo"} size={width}/>
-          <Button color={"green"} text={"Submit"} size={width}/>
-            </form>
+            <label className={styles.name} htmlFor="title">
+              Title
+            </label>
+            <input
+              className={styles.input}
+              type="text"
+              id="title"
+              name="title"
+              value={inputData.title}
+              onChange={(e) => handleInputChange("title", e.target.value)}
+            />
+
+            <div className={styles.textarea}>
+              <label className={styles.name} htmlFor="content">
+                Content
+              </label>
+              <textarea
+                className={styles.area}
+                id="content"
+                name="content"
+                value={inputData.content}
+                onChange={(e) => handleInputChange("content", e.target.value)}
+              ></textarea>
+            </div>
+
+            <label className={styles.name}>Select an image</label>
+                  <input
+                    className={styles.input}
+                    type="file"
+                    name="image"
+                    onChange={handleImageChange}
+                  />
+                  {existingData.image && (
+                    <img
+                      src={`http://localhost:5000/${existingData.image}`}
+                      alt="Current Image"
+                      className={styles.currentImage}
+                    />
+                  )}
+
+            <Button color={"green"} text={"Submit"} size={width} />
+          </form>
         </div>
 
         <div className={styles.photo}>
           <img className={styles.img} src={photo} alt="Mail Rafiki" />
         </div>
-      </div >
+      </div>
 
-     
-        <ScrollButton/>
+      <ScrollButton />
     </div>
   );
-}
+};
 
 export default BlogUpdate;
--- technow/src/Pages/NewsForm/NewsForm.jsx
+++ technow/src/Pages/NewsForm/NewsForm.jsx
@@ -3,6 +3,7 @@ import styles from "./NewsForm.module.css";
 import photo from "../../Assets/Images/Mail.png"
 import { useState , useEffect } from "react";
 import { ScrollButton } from "../../Components/ScrollButton/ScrollButton";
+import axios from "axios";
 
 const NewsForm = () => {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -19,12 +20,63 @@ const NewsForm = () => {
           window.removeEventListener('resize', handleResize);
       };
   }, []);
+
+  const [categoriesData, setCategoriesData] = useState([]);
+
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/read/category');
+        setCategoriesData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCategory();
+  }, []);
+  
+  const [newsletter , setNewsletter] = useState([])
+  useEffect(() => {
+    const fetchNewsletter = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/read/newsletter');
+        setNewsletter(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchNewsletter();
+  }, []);
+
+  const [success , setSuccess] = useState(false)
+  const [failure , setFailure ] = useState(false)
+
+   const addNews=(e)=>{
+    e.preventDefault();
+    const formData= new FormData(e.target);
+    axios.post("http://localhost:5000/add/news", formData)
+    .then((respone)=>{
+      console.log("Request sent successfully", respone.data);
+      if(respone.ok){
+        setSuccess(true)
+      }else{
+        setSuccess(false)
+        setFailure(true)
+      }
+    })
+    .catch((error) =>{
+      console.log(error)
+    })
+   }
+    
   return (
     <div className={styles.Container}>
       <h1 className={styles.h1}>Add News</h1>
       <div className={styles.Top}>
         <div className={styles.Left}>
-          <form className={styles.form} action="">
+          <form className={styles.form} action="" onSubmit={addNews}>
 
               <label className={styles.name} htmlFor="author">Author</label>
               <input className={styles.input}  type="text" id="author" name="author" />
@@ -33,11 +85,56 @@ const NewsForm = () => {
               <label className={styles.name} htmlFor="title">Title</label>
               <input className={styles.input} type="text" id="title" name="title" />
 
+
+              <label className={styles.name} htmlFor="Category">Category:</label>
+              <select className={styles.input} name="Category" id="Category">
+                {categoriesData.map((category) => (
+                  <option key={category._id} value={category._id}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+
+
+              <label className={styles.name} htmlFor="newsletter">Newsletter:</label>
+              <select className={styles.input} name="newsletterID" id="newsletter">
+                {newsletter.map((newsletter) => (
+                  <option key={newsletter._id} value={newsletter._id}>
+                    {newsletter.name}
+                  </option>
+                ))}
+              </select>
+
               <label className={styles.name} htmlFor="date">Date</label>
               <input className={styles.input}  type="date" id="date" name="date" />
 
               <label className={styles.name} htmlFor="subtitle">Subtitle</label>
               <input className={styles.input}  type="text" id="subtitle" name="subtitle" />
+              <div className={styles.Bottom}>
+          <div className={styles.textarea}>
+            <label  className={styles.name} htmlFor="description">description</label>
+            <textarea className={styles.area} id="description" name="description"></textarea>
+          </div>
+          <div className={styles.textarea}>
+            <label  className={styles.name} htmlFor="subtitleDescription">Subtitle Content</label>
+            <textarea className={styles.area} id="subtitleDescription" name="subtitleDescription"></textarea>
+          </div>
+          <div className={styles.textarea} >
+            <label  className={styles.name} htmlFor="links">Additional Links</label>
+            <textarea className={`${styles.area} ${styles.links}`} id="links" name="links"></textarea>
+          </div>
+        </div>
+        <div className={styles.inputContainer}>
+            <label className={styles.label}>Enter an image</label>
+            <input
+              className={styles.input}
+              type="file"
+              name="image"/>
+          </div>
+        <div className={styles.btn}>
+          <Button color={"red"} text={"Upload photo"} size={width}/>
+          <Button color={"green"} text={"Add News"} size={width}/>
+        </div>
 
             </form>
         </div>
@@ -47,24 +144,7 @@ const NewsForm = () => {
         </div>
       </div >
 
-        <div className={styles.Bottom}>
-          <div className={styles.textarea}>
-            <label  className={styles.name} htmlFor="content">Content</label>
-            <textarea className={styles.area} id="content" name="content"></textarea>
-          </div>
-          <div className={styles.textarea}>
-            <label  className={styles.name} htmlFor="subcontent">Subtitle Content</label>
-            <textarea className={styles.area} id="subcontent" name="subcontent"></textarea>
-          </div>
-          <div className={styles.textarea} >
-            <label  className={styles.name} htmlFor="links">Additional Links</label>
-            <textarea className={`${styles.area} ${styles.links}`} id="links" name="links"></textarea>
-          </div>
-        </div>
-        <div className={styles.btn}>
-          <Button color={"red"} text={"Upload photo"} size={width}/>
-          <Button color={"green"} text={"Add News"} size={width}/>
-        </div>
+       
         <ScrollButton/>
     </div>
   );
