9a3fe8792a8f3619cbd9541a369bf93e575ff535
abdulaziz <aboudecharkawi@gmail.com>
2023-11-07 09:13:58 +0200

    adding the data using axios in blogForm

--- technow/src/Pages/BlogsForm/BlogsForm.jsx
+++ technow/src/Pages/BlogsForm/BlogsForm.jsx
@@ -3,7 +3,8 @@ import styles from "./BlogsForm.module.css"; // Import the CSS module
 import photo from "../../Assets/Images/Mail.png"
 import { ScrollButton } from "../../Components/ScrollButton/ScrollButton";
 import { Button } from "../../Components/Buttons/Buttons";
-
+import axios from "axios";
+import {Link} from 'react-router-dom'
 
 const BlogForm = () => {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -21,6 +22,29 @@ const BlogForm = () => {
       };
   }, []);
 
+const [success , setSuccess] = useState(false)
+const [failure , setFailure ] = useState(false)
+
+  const addBlog=(e)=>{
+    e.preventDefault();
+    const formData= new FormData(e.target);
+
+      axios.post("http://localhost:5000/add/blogs", formData)
+      .then((response)=>{
+        console.log("Request sent successfully", response.data);
+        if(response.ok){
+          setSuccess(true)
+        }else{
+          setSuccess(false)
+          setFailure(true)
+        }
+      })
+      .catch((error) =>{
+        console.log(error )
+      })
+    
+  }
+
   return (
     <div>
      
@@ -31,18 +55,41 @@ const BlogForm = () => {
       <h1 className={styles.h1}>Add Blog</h1>
       <div className={styles.Top}>
         <div className={styles.Left}>
-          <form className={styles.form} action="">
+          <form className={styles.form} action="" onSubmit={addBlog}>
 
               <label className={styles.name} htmlFor="fullname">Full Name</label>
-              <input className={styles.input}  type="text" id="fullname" name="fullname" />
+              <input className={styles.input}  type="text" id="fullname" name="author" />
 
 
               <label className={styles.name} htmlFor="title">Title</label>
               <input className={styles.input} type="text" id="title" name="title" />
 
+ 
+            
 
-              <label className={styles.name} htmlFor="date">Date</label>
-              <input className={styles.input}  type="text" id="date" name="date" />
+        <div className={styles.Bottom}>
+          <div className={styles.textarea}>
+            <label  className={styles.name} htmlFor="content">Content</label>
+            <textarea className={styles.area} id="content" name="content"></textarea>
+          </div>
+
+          <div className={styles.inputContainer}>
+            <label className={styles.label}>Enter an image</label>
+            <input
+              className={styles.input}
+              type="file"
+              name="image"/>
+          </div>
+        <Button color={"red"} size={width} text={"Post Now"}/>
+        {success === true ? (
+            <p>Blog added successfully</p>
+        ): failure === true ? (
+            <p>An error occured </p>
+        ): ""}
+        <Link to='/blog'><Button color={"green"} text={"Go back to Blogs"} size={"big"}/></Link>
+        </div>
+              
+              
 
             </form>
         </div>
@@ -52,15 +99,7 @@ const BlogForm = () => {
         </div>
       </div >
 
-        <div className={styles.Bottom}>
-          <div className={styles.textarea}>
-            <label  className={styles.name} htmlFor="content">Content</label>
-            <textarea className={styles.area} id="content" name="content"></textarea>
-          </div>
-
-        <Button color={"green"} size={width} text={"Upload Photo"} />
-        <Button color={"red"} size={width} text={"Post Now"}/>
-        </div>
+       
     </div>
       <ScrollButton/>
     </div>
