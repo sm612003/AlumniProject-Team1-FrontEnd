db9fb5f5d6eef109361333e64b4a1e5c4756e700
RayanAlHassan <rayanalhassan2000@gmail.com>
2024-01-01 11:23:18 +0200

    password toggle , handle login case if already loged in

--- technow/src/Pages/Login/Login.js
+++ technow/src/Pages/Login/Login.js
@@ -2,23 +2,23 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../../firebase";
 
-import React, { useContext, useState,useRef, useEffect } from "react";
+import React, { useContext, useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import styles from "./Login.module.css"; // Import your styles
-import {  AuthContext } from "../../Context/AuthContext";
-
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { AuthContext } from "../../Context/AuthContext";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
-  const { setUser } = useContext(AuthContext); // Use the useContext hook to access setUser
+  const { setUser, user } = useContext(AuthContext); // Use the useContext hook to access setUser and state
   const navigate = useNavigate();
-  //network err
-  const [networkError, setNetworkError] = useState(false);
+  const [networkError, setNetworkError] = useState(false); //network err
   const [isLoading, setIsLoading] = useState(false);
-    const showToastRef = useRef(false);
+  const showToastRef = useRef(false);
 
+  //toast message for login page
   const showToastMessage = () => {
     toast.success("Please Log In To Add Blog !", {
       position: toast.POSITION.TOP_RIGHT,
@@ -27,16 +27,14 @@ const Login = () => {
       pauseOnHover: true,
     });
   };
-
-  // Call showToastMessage when the component mounts
+  // Call showToastMessage when the component mounts (on the open of login page the toast will appear)
   useEffect(() => {
     if (!showToastRef.current) {
       showToastMessage();
       showToastRef.current = true;
     }
   }, []);
-
-  
+  // handle Network err
   useEffect(() => {
     const handleOffline = () => {
       setNetworkError(true);
@@ -48,6 +46,67 @@ const Login = () => {
       window.removeEventListener("offline", handleOffline);
     };
   }, []);
+
+  // LOGIN form handle input change
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  // login process
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!navigator.onLine) {
+      setNetworkError(true);
+      setError(false);
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const response = await axios.post(
+        "http://localhost:5000/user/login",
+        formData
+      );
+
+      if (response.data) {
+        setUser(response.data); // Assuming user data is nested under response.data
+        console.log("role: " + response.data.role);
+        console.log("responce and data of login", response.data);
+        console.log("responce of login", response);
+        console.log("localstorage :", localStorage);
+        if (response.data.role === "admin") {
+          navigate("/dashboard");
+        } else {
+          navigate("/blogsForm");
+        }
+        setLoading(false);
+      }
+    } catch (error) {
+      if (error.message === "Network request failed") {
+        setNetworkError(true);
+        setIsLoading(false);
+      } else {
+        console.log("err", error);
+        setError(true);
+        setErrorMessage("Invalid email or password");
+        setLoading(false);
+      }
+    }
+  };
+  // Google login process
   const handleOAuth = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -73,76 +132,37 @@ const Login = () => {
           }
         });
     } catch (err) {
-
       console.log("OAuth: ", err);
     }
   };
-
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    if (!navigator.onLine) {
-      setNetworkError(true);
-      setError(false);
-      setIsLoading(false);
-      return;
-    }
-
-    try {
-      setLoading(true);
-      const response = await axios.post(
-        "http://localhost:5000/user/login",
-        formData
-      );
-
-      console.log(response.data);
-      console.log(response);
-
-      if (response.data) {
-     
-        setUser(response.data); // Assuming user data is nested under response.data
-        console.log("role: " + response.data.role);
-
-        if (response.data.role === "admin") {
+  //CHECK the user if LOGED IN BEFORE dont ask for another login every time need to add blog
+  useEffect(() => {
+    const checkLoggedInUser = async () => {
+      if (localStorage.getItem("authUser") && user) {
+        // If authUser exists in localStorage and user is not null,
+        // it means the user is already logged in.
+        // You can redirect them to the appropriate page.
+        if (user.role === "admin") {
           navigate("/dashboard");
         } else {
           navigate("/blogsForm");
         }
-        setLoading(false);
       }
-    } catch (error) {
-      if (error.message === "Network request failed") {
-        setNetworkError(true);
-        setIsLoading(false);
-      } else {
-        console.log("err", error);
-        setError(true);
-        setErrorMessage("Invalid email or password");
-        setLoading(false);
-      }
-    }
-  };
+    };
+    checkLoggedInUser();
+  }, [user]); // Run this effect whenever the user state changes
 
+  // PASSWOED HIDE AND SHOW
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleTogglePassword = () => {
+      setShowPassword(!showPassword);
+    };
+    const passwordInputType = showPassword ? "text" : "password";
   return (
     <body>
       <div className={styles.container}>
-      <ToastContainer></ToastContainer>
+        <ToastContainer></ToastContainer>
         <div className={styles["login-container"]}>
           <form onSubmit={handleLogin} className={styles["login-form"]}>
             <h2>Login</h2>
@@ -158,15 +178,24 @@ const Login = () => {
               />
             </div>
             <div className={styles["form-group"]}>
-              <label>Password</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
+              <div className={styles["passwordInputContainer"]}>
+                <label>Password</label>
+                <input
+                  type={passwordInputType}
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                  disabled={loading}
+                />
+                <div
+                  className={styles["password-toggle"]}
+                  onClick={handleTogglePassword}
+                >
+                  {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+                </div>
+              </div>
             </div>
             <button className={styles.logbtn} type="submit">
               Log in
